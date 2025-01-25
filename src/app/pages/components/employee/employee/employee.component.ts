97c9b71d71a3df78e8aedc277f9c37732a10eaa6
Ajnas kp <ajnaskp67@gmail.com>
2025-01-25 15:54:59 +0530

    feat: sidebar

--- src/app/pages/components/employee/employee/employee.component.ts
+++ src/app/pages/components/employee/employee/employee.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { EmployeetableComponent } from '../employeetable/employeetable.component';
+import { SidebarComponent } from '../../../../componets/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [EmployeetableComponent,TableModule],
+  imports: [EmployeetableComponent, TableModule, SidebarComponent],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.scss'
 })
